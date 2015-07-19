0fa601c39d3d4d8892384bdc96160f5721ea230c
Stian Håklev <shaklev@gmail.com>
2015-07-19 18:01:03 +0800

    Only exporting what original exports

--- lib/text.ex
+++ lib/text.ex
@@ -14,12 +14,14 @@ defmodule Text do
   # Cursors are either a single number (which is the cursor position) or a pair of
   # [anchor, focus] (aka [start, end]). Be aware that end can be before start.
 
-  def exportsCreate(initial) when is_binary(initial), do: initial
+  def create(initial) when is_binary(initial), do: initial
 
   # Check the operation is valid. Throws if not valid.
   #--------------------------------------------------------------------------------  
-  mdef checkOp do
+  mdefp checkOp do
     [] -> nil
+
+    [x] when is_integer(x) -> raise "Op has a trailing skip"
 
     [ %{d: x} | rest ] when is_integer(x) ->
       if !(x > 0), do: raise "Object components must be deletes of size > 0"
@@ -39,7 +41,7 @@ defmodule Text do
   end
   #--------------------------------------------------------------------------------  
 
-  mdef checkSelection do
+  mdefp checkSelection do
     [ selection1, selection2 | _ ] 
       when is_integer(selection1) and is_integer(selection2) -> nil
 
@@ -47,7 +49,7 @@ defmodule Text do
   end 
 
   # from makeAppend which returns a function
-  mdef append do
+  mdefp append do
     component, []                                        -> [ component ]
     nil, acc                                             -> acc
     %{d: 0}, acc                                         -> acc
@@ -58,133 +60,136 @@ defmodule Text do
   end
 
   #-------------------------------------------------------------------------------- 
-  def exportsTransform(op, otherOp, side) do
+  def transform(op, otherOp, side) do
     if side != "left" && side != "right", do: raise "Side must be left or right"
 
     checkOp(op)
     checkOp(otherOp)
 
-    state = {op, 0, 0, side} # { op, idx, offset, side }
-    { acc, state } = Enum.reduce(otherOp, {[], state}, &exportsTransformApply/2)
+    state = %{op: op, idx: 0, offset: 0, side: side} 
+    { state, acc } = Enum.reduce(otherOp, {state, []}, &transformApply/2)
     
-    acc = takeFinal(state, acc)
-    trim(acc)
+    acc = takeFinal({state, acc})
+    |> Enum.reverse
+    |> trim
   end
 
-  #-------------------------------------------------------------------------------- 
-  def takeFinal(state = {op, idx, offset, side}, acc) do
-    { result, idx, offset } = take(state, -1)
-    if is_nil(result) do
-      acc
-    else
-      acc = append(acc, result)
-      takeFinal({op, idx, offset, side}, acc)
-    end
-  end
 
-  mdef exportsTransformApply do
-    x, {acc, state = {op, idx, offset, side}} when is_integer(x) ->
-      {{ result, idx, offset }, acc } = takeInteger(state, acc, x)
-      {acc, {op, idx, offset, side}}
+  mdefp transformApply do
+    x, {state, acc} when is_integer(x) ->
+      takeInteger({state, acc}, x)
       
-    x, {acc, state = {op, idx, offset, side}} when is_binary(x) ->
-      if side == "left" do
+    x, {state, acc} when is_binary(x) ->
+      if state.side == "left" do
         if is_binary(peek(state)) do
           { result, idx, offset } = take(state, -1)
+          state = %{ state | idx: idx, offset: offset }
           acc = append(result, acc)
         end
       else
         acc = append(String.length(x), acc)
       end
-      {acc, {op, idx, offset, side}}
+      {state, acc}
         
-    %{d: d}, {acc, state = {op, idx, offset, side}} ->
-      {{ result, idx, offset }, acc } = takeInteger(state, acc, d)
-      {acc, {op, idx, offset, side}}
+    %{d: d}, {state, acc} ->
+      takeDelete({state, acc}, d)
+  end
+  #-------------------------------------------------------------------------------- 
+
+  defp takeFinal({state, acc}) do
+    { result, idx, offset } =take(state, -1)
+    if is_nil(result) do
+      acc
+    else
+      acc = append(result, acc)
+      takeFinal { %{ state | idx: idx, offset: offset}, acc }
+    end
   end
 
-  mdef takeDelete do
-    state = {op, idx, offset, side}, acc, x when x > 0 ->
+  mdefp takeDelete do
+    {state, acc}, x when x > 0 ->
       { result, idx, offset } = take(state, x, "i")
       case result do
         y when is_integer(y) -> x = x - y
         y when is_binary(y) -> acc = append(y, acc)
         %{d: y} -> x = x - y
       end
-      takeDelete {op, idx, offset, side}, acc, x
+      takeDelete({ %{ state | idx: idx, offset: offset }, acc }, x)
 
-    state, acc, x -> {state, acc}
+    {state, acc}, x -> {state, acc}
   end
 
-  mdef takeInteger do
-    state = {op, idx, offset, side}, acc, x when x > 0 ->
+  mdefp takeInteger do
+    {state, acc}, x when x > 0 ->
       { result, idx, offset } = take(state, x, "i")
       acc = append(result, acc)
       if is_binary(result), do: x = x - componentLength(result)
-      takeInteger {op, idx, offset, side}, acc, x
+      takeInteger { %{ state| idx: idx, offset: offset}, acc }, x
 
-    state, acc, x -> {state, acc}
+    {state, acc}, x -> {state, acc}
   end
   
   #-------------------------------------------------------------------------------- 
-  def take({op, idx, offset, side}, n, indivisableField \\ nil) do
+  def take(state = %{op: op, idx: idx, offset: offset }, n, 
+    indivisableField \\ nil) do
+    IO.inspect(state)
+
     if idx == length(op) do
-      if n = -1, do: nil, else: n
+      res = if n == -1, do: nil, else: n
+      { res, idx, offset }
     else
-      takeApply({Enum.at(op, idx), idx, offset}, n, indivisableField)
+      takeApply(%{ state | op: Enum.at(op, idx) }, n, indivisableField)
     end
   end
 
-  mdef takeApply do
-    state = {c, idx, offset}, n, indivisableField when is_integer(c) ->
+  mdefp takeApply do
+    %{op: c, idx: idx, offset: offset}, n, indivisableField when is_integer(c) ->
       if n == -1 || c - offset <= n do
         {c - offset, idx + 1, 0}
       else
         {n, idx, offset + n}
       end
-    state = {c, idx, offset}, n, indivisableField when is_binary(c) ->
+    %{op: c, idx: idx, offset: offset}, n, indivisableField when is_binary(c) ->
       if n == -1 || indivisableField == "i" || String.length(c) - offset <= n do
         { String.slice(c, offset, 9999999), idx + 1, 0 }
       else
-        { String.slice(c, offset, offset + n), idx, offset + n }
+        { String.slice(c, offset, n), idx, offset + n }
       end
-    state = {%{d: d}, idx, offset}, n, indivisableField -> 
-      if n == 1 || indivisableField == "d" || d - offset <= n do
+    %{ op: %{d: d}, idx: idx, offset: offset}, n, indivisableField -> 
+      if n == -1 || indivisableField == "d" || d - offset <= n do
         { %{d: d - offset }, idx + 1, 0 }
       else
         { %{d: n}, idx, offset + n }
       end
   end
 
-  def peek({op, idx, offset, side}), do: Enum.at(op, idx)
+  defp peek(%{ op: op, idx: idx }), do: Enum.at(op, idx)
   
   #-------------------------------------------------------------------------------- 
-  def exportsCompose(op1, op2) do
+  def compose(op1, op2) do
     checkOp(op1)
     checkOp(op2)
 
-    state = { Enum.at(op1, 0), 0, 0, "" }
-    { acc, state } = Enum.reduce(op2, {[], state}, &exportsComposeApply/2)
+    state = %{ op: Enum.at(op1, 0), idx: 0, offset: 0 }
+    { state, acc } = Enum.reduce(op2, {state, []}, &composeApply/2)
     
-    acc = takeFinal(state, acc)
+    acc = takeFinal({state, acc})
     trim(acc)
   end
 
-  mdef exportsComposeApply do
-    x, {acc, state = {op, idx, offset, side}} when is_integer(x) ->
-      {{ result, idx, offset }, acc } = takeInteger(state, acc, x)
-      {acc, {op, idx, offset, side}}
-    x, {acc, state = {op, idx, offset, side}} when is_binary(x) ->
+  mdefp composeApply do
+    x, {state, acc} when is_integer(x) ->
+      takeInteger({ state, acc }, x)
+    x, {state, acc} when is_binary(x) ->
       acc = append(x, acc)
-      {acc, {op, idx, offset, side}}
-    %{d: x}, {acc, state = {op, idx, offset, side}} ->
-      {{ result, idx, offset }, acc } = takeComposeDelete(state, acc, x)
-      {acc, {op, idx, offset, side}}
+      { state, acc }
+    %{d: x}, {state, acc} ->
+      takeComposeDelete( {state, acc }, x)
   end
 
 
-  mdef takeComposeDelete do
-    state = {op, idx, offset, side}, acc, x when x > 0 ->
+  mdefp takeComposeDelete do
+    { state, acc }, x when x > 0 ->
       { result, idx, offset } = take(state, x, "d")
       case result do
         y when is_integer(y) -> 
@@ -193,12 +198,12 @@ defmodule Text do
         y when is_binary(y) -> x = x - String.length(y)
         %{d: y} -> acc = append(result, acc)
       end
-      takeComposeDelete {op, idx, offset, side}, acc, x
+      takeComposeDelete { %{state | idx: idx, offset: offset }, acc }, x
 
-    state, acc, x -> {state, acc}
+    {state, acc}, x -> {state, acc}
   end
 
-  mdef takeComposeInteger do
+  mdefp takeComposeInteger do
     state = {op, idx, offset, side}, acc, x when x > 0 ->
       { result, idx, offset } = take(state, x, "d")
       acc = append(result, acc)
@@ -208,7 +213,7 @@ defmodule Text do
     state, acc, x -> {state, acc}
   end
   #-------------------------------------------------------------------------------- 
-  mdef componentLength do
+  mdefp componentLength do
     c when is_integer(c) -> c
     c when is_binary(c)  -> String.length(c)
     c when is_list(c)    -> length(c)
@@ -216,19 +221,19 @@ defmodule Text do
 
   # Trim any excess skips from the end of an operation.
   # There should only be at most one, because the operation was made with append.
-  mdef trim do
+  mdefp trim do
     [ x | rest ] when is_integer(x) -> rest
     x                               -> x
   end
 
-  def exportsNormalize(op) do
+  def normalize(op) do
     Enum.reduce(op, [], &append/2)
     |> trim
     |> Enum.reverse
   end
 
   #-------------------------------------------------------------------------------- 
-  def exportsApply(str, op) when is_binary(str) do
+  def apply(str, op) when is_binary(str) do
     checkOp op
     Enum.reduce(op, {str, []}, &applyOp/2)
     |> elem(1)
@@ -236,7 +241,7 @@ defmodule Text do
     |> IO.iodata_to_binary
   end
 
-  mdef applyOp do
+  mdefp applyOp do
     %{d: d}, {str, acc}                -> { String.slice(str, d, 999999), acc }
     op, {str, acc} when is_binary(op)  -> { str, [ op | acc ] }
     op, {str, acc} when is_integer(op) ->
@@ -245,9 +250,9 @@ defmodule Text do
   end
   #-------------------------------------------------------------------------------- 
 
-  def transformPosition(cursor, op), do: transformPositionPrime(cursor, op, 0)
+  defp transformPosition(cursor, op), do: transformPositionPrime(cursor, op, 0)
 
-  mdef transformPositionPrime do
+  mdefp transformPositionPrime do
     cursor, [], _ -> cursor
     cursor, _, pos when cursor <= pos -> cursor
 
@@ -263,7 +268,7 @@ defmodule Text do
   end
   #-------------------------------------------------------------------------------- 
 
-  def exportsTransformSelection(selection, op, isOwnOp) do
+  def transformSelection(selection, op, isOwnOp) do
     if isOwnOp do
     # Just track the position. We'll teleport the cursor to the end anyway.
     # This works because text ops don't have any trailing skips at the end - so the last
@@ -274,21 +279,21 @@ defmodule Text do
     end
   end
   
-  mdef applyOwnSelection do
+  mdefp applyOwnSelection do
     c, acc when is_integer(c) -> acc + c
-    c, acc when is_binary(c)  -> acc + Enum.length(c)
+    c, acc when is_binary(c)  -> acc + String.length(c)
   end
 
-  mdef applySelection do
+  mdefp applySelection do
     selection, op when is_integer(selection) -> transformPosition(selection, op)
     [ sel0, sel1 | _ ], op                   -> [ transformPosition(sel0, op), transformPosition(sel1, op) ]
   end
   #-------------------------------------------------------------------------------- 
 
   
-  # TODO: Hope I got the logic right, defintively could use some tests, if I knew
+  # TODO: Hope I got the logic right, defpintively could use some tests, if I knew
   # what the desired result was
-  def exportsSelectionEq(c1, c2) do
+  def selectionEq(c1, c2) do
     c1 = take_first c1
     c2 = take_first c2
     [ c1_0, c1_1 | _ ] = c1
@@ -296,7 +301,7 @@ defmodule Text do
     c1 == c2 || ( !is_nil(c1_0) && !is_nil(c2_0) && c1_0 == c2_0 && c1_1 == c2_1 )
   end
 
-  mdef take_first do
+  mdefp take_first do
     [x, y | _], _ when not is_nil(x) and x == y -> x
     c -> c
   end
