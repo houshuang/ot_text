231d4d9bd6ee29065086818bd7e5282eee2f56dd
Stian Håklev <shaklev@gmail.com>
2015-07-25 14:14:53 +0800

    Passing should normalize sanely

--- lib/text.ex
+++ lib/text.ex
@@ -16,11 +16,11 @@ defmodule Text do
 
   # Exported functions
 
-  # apply/2          : check     
-  # normalize/1      : check          
-  # transform/3             
-  # compose/2        : check           
-  # selectionEq/2        
+  # apply/2          : check
+  # normalize/1      : check
+  # transform/3
+  # compose/2        : check
+  # selectionEq/2
   # transformSelection/3
   # create/1         : check
   # take/3
@@ -28,7 +28,7 @@ defmodule Text do
   def create(initial) when is_binary(initial), do: initial
 
   # Check the operation is valid. Throws if not valid.
-  #--------------------------------------------------------------------------------  
+  #--------------------------------------------------------------------------------
   mdefp checkOp do
     [] -> nil
 
@@ -39,7 +39,7 @@ defmodule Text do
       checkOp rest
 
     [ x | rest ] when is_binary(x) ->
-      if !(String.length(x) > 0), do: raise "Inserts cannot be empty" 
+      if !(String.length(x) > 0), do: raise "Inserts cannot be empty"
       checkOp rest
 
     [ x, y | rest ] when is_integer(x) and is_integer(y) ->
@@ -50,34 +50,36 @@ defmodule Text do
       if !(x > 0), do: raise "Skip components must be >0"
       checkOp rest
   end
-  #--------------------------------------------------------------------------------  
+  #--------------------------------------------------------------------------------
 
   mdefp checkSelection do
-    [ selection1, selection2 | _ ] 
+    [ selection1, selection2 | _ ]
       when is_integer(selection1) and is_integer(selection2) -> nil
 
     x when is_integer(x) -> nil
-  end 
+  end
 
   # from makeAppend which returns a function
   mdefp append do
-    component, []                                        -> [ component ]
+    "", acc                                              -> acc
     nil, acc                                             -> acc
     %{d: 0}, acc                                         -> acc
+    0, acc                                               -> acc
+    component, []                                        -> [ component ]
     %{d: x}, [ %{d: y} | rest ]                          -> [ %{d: x + y} | rest ]
     x, [ y | rest ] when is_integer(x) and is_integer(y) -> [ y + x | rest ]
     x, [ y | rest ] when is_binary(x) and is_binary(y)   -> [ y <> x | rest ]
     component, acc                                       -> [ component | acc ]
   end
 
-  #-------------------------------------------------------------------------------- 
+  #--------------------------------------------------------------------------------
   def transform(op, otherOp, side) do
     if side != "left" && side != "right", do: raise "Side must be left or right"
 
     checkOp(op)
     checkOp(otherOp)
 
-    state = %{ op: op, idx: 0, offset: 0, side: side} 
+    state = %{ op: op, idx: 0, offset: 0, side: side}
     { state, acc } = Enum.reduce(otherOp, {state, []}, &transformApply/2)
     acc = takeFinal({state, acc})
     |> Enum.reverse
@@ -88,7 +90,7 @@ defmodule Text do
   mdefp transformApply do
     x, {state, acc} when is_integer(x) ->
       takeInteger({state, acc}, x)
-      
+
     x, {state, acc} when is_binary(x) ->
       if state.side == "left" do
         if is_binary(peek(state)) do
@@ -100,11 +102,11 @@ defmodule Text do
         acc = append(String.length(x), acc)
       end
       {state, acc}
-        
+
     %{d: d}, {state, acc} ->
       takeDelete({state, acc}, d)
   end
-  #-------------------------------------------------------------------------------- 
+  #--------------------------------------------------------------------------------
 
   defp takeFinal({state, acc}) do
     { result, idx, offset } =take(state, -1)
@@ -138,9 +140,9 @@ defmodule Text do
 
     {state, acc}, x -> {state, acc}
   end
-  
-  #-------------------------------------------------------------------------------- 
-  def take(state = %{op: op, idx: idx, offset: offset}, 
+
+  #--------------------------------------------------------------------------------
+  def take(state = %{op: op, idx: idx, offset: offset},
     n, indivisableField \\ nil) do
 
     if idx == length(op) do
@@ -164,7 +166,7 @@ defmodule Text do
       else
         { String.slice(c, offset, n), idx, offset + n }
       end
-    %{c: %{d: d}, idx: idx, offset: offset}, n, indivisableField -> 
+    %{c: %{d: d}, idx: idx, offset: offset}, n, indivisableField ->
       if n == -1 || indivisableField == "d" || d - offset <= n do
         { %{d: d - offset }, idx + 1, 0 }
       else
@@ -173,14 +175,14 @@ defmodule Text do
   end
 
   defp peek(%{ op: op, idx: idx }), do: Enum.at(op, idx)
-  
-  #-------------------------------------------------------------------------------- 
+
+  #--------------------------------------------------------------------------------
   def compose(op1, op2) do
     checkOp(op1)
     checkOp(op2)
 
-    state = %{ op: op1, idx: 0, offset: 0 } 
-      
+    state = %{ op: op1, idx: 0, offset: 0 }
+
     { state, acc } = Enum.reduce(op2, {state, []}, &composeApply/2)
     takeFinal({state, acc})
     |> Enum.reverse
@@ -202,7 +204,7 @@ defmodule Text do
     { state, acc }, x when x > 0 ->
       { result, idx, offset } = take(state, x, "d")
       case result do
-        y when is_integer(y) -> 
+        y when is_integer(y) ->
           x = x - y
           acc = append(%{d: result}, acc)
         y when is_binary(y) -> x = x - String.length(y)
@@ -222,7 +224,7 @@ defmodule Text do
 
     {state, acc}, x -> {state, acc}
   end
-  #-------------------------------------------------------------------------------- 
+  #--------------------------------------------------------------------------------
   mdefp componentLength do
     c when is_integer(c)    -> c
     c when is_binary(c)     -> String.length(c)
@@ -246,11 +248,11 @@ defmodule Text do
 
   def normalize(op) do
     Enum.reduce(op, [], &append/2)
-    |> trim
     |> Enum.reverse
+    |> trim
   end
 
-  #-------------------------------------------------------------------------------- 
+  #--------------------------------------------------------------------------------
   def apply(str, op) when is_binary(str) do
     checkOp op
     {str, acc} = Enum.reduce(op, {str, []}, &applyOp/2)
@@ -267,7 +269,7 @@ defmodule Text do
       if op > String.length(str), do: raise "The op is too long for this document"
       { String.slice(str, op, 999999),  [ String.slice(str, 0, op) | acc ] }
   end
-  #-------------------------------------------------------------------------------- 
+  #--------------------------------------------------------------------------------
 
   defp transformPosition(cursor, op), do: transformPositionPrime(cursor, op, 0)
 
@@ -285,7 +287,7 @@ defmodule Text do
     cursor, [%{d: d} | rest], pos ->
       transformPositionPrime(cursor - min(d, cursor - pos), rest, pos)
   end
-  #-------------------------------------------------------------------------------- 
+  #--------------------------------------------------------------------------------
 
   def transformSelection(selection, op, isOwnOp) do
     if isOwnOp do
@@ -297,7 +299,7 @@ defmodule Text do
       applySelection(selection, op)
     end
   end
-  
+
   mdefp applyOwnSelection do
     c, acc when is_integer(c) -> acc + c
     c, acc when is_binary(c)  -> acc + String.length(c)
@@ -307,9 +309,9 @@ defmodule Text do
     selection, op when is_integer(selection) -> transformPosition(selection, op)
     [ sel0, sel1 | _ ], op                   -> [ transformPosition(sel0, op), transformPosition(sel1, op) ]
   end
-  #-------------------------------------------------------------------------------- 
+  #--------------------------------------------------------------------------------
 
-  
+
   # TODO: Hope I got the logic right, defpintively could use some tests, if I knew
   # what the desired result was
   def selectionEq(c1, c2) do
